d3a73c4ce93516949b8fac0885917327a78ab0b4
mstkpln <contactmstkpln@gmail.com>
2021-09-13 00:34:50 -0400

    first commit

---
+++ MetotChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MetotChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frank= "Frank";
+            string zoe = "Zoe";
+            string john = "John";
+            GF(frank, zoe, john);
+
+        } public static void GF(string friendName1, string friendName2, string friendName3)
+        {
+            Console.WriteLine("Hello {0}, my friend!", friendName1);
+            Console.WriteLine("Hello {0}, my friend!", friendName2);
+            Console.WriteLine("Hello {0}, my friend!", friendName3);
+
+        }
+    }
+}
